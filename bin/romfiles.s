c4ebe3d103fcd3f0b6c87d62892402ed397032c6
Larry <kholdfuzion@gmail.com>
2017-08-16 09:31:39 -0500

    more renaming, fix improperly split file
    Former-commit-id: d7fc6b06200a7b4252ad068e08b4cee255ef867a [formerly 80cec1a19b591902cac22b1c835354526102e2ac] [formerly aefbbe23ce9da64689751e8fbc32598165e1b2a9 [formerly ffe6348a4edb3284ea685758e272440681bea1a5]]
    Former-commit-id: 3162e058eeee4bce5a1aa05acac960ccefd66493 [formerly 3748ed271b0bd5ac49b54cd85fc44927d7817fd3]
    Former-commit-id: b266883c808108c2bf6b6c4ee43dfb6bee590a9a

--- bin/romfiles.s
+++ bin/romfiles.s
@@ -1,19 +1,31 @@
 .section .romfiles, "a"
+
+.global unknown
 unknown:
 .incbin "bin/ge007.u.117880.unknown.bin"
 unknown_end:
+
+.global Globalimagetable
 Globalimagetable:
 .incbin "bin/ge007.u.29D160.Globalimagetable.bin"
 Globalimagetable_end:
+
+.global Globalimagetable_commandblock
 Globalimagetable_commandblock:
 .incbin "bin/ge007.u.29E560.Globalimagetable_commandblock.bin"
 Globalimagetable_commandblock_end:
+
+.global unknown1
 unknown1:
 .incbin "bin/ge007.u.2A0560.unknown1.bin"
 unknown1_end:
+
+.global unknown2
 unknown2:
 .incbin "bin/ge007.u.2A4D50.unknown2.bin"
 unknown2_end:
+
+.global unknown3
 unknown3:
 .incbin "bin/ge007.u.2AE280.unknown3.bin"
 unknown3_end:
